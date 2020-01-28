234bbf03a3fa78973850df896c33217e132edc55
sky94520 <541052067@qq.com>
2020-01-28 21:23:50 +0800

    添加了pymysql的引用

--- models.py
+++ models.py
@@ -3,6 +3,7 @@
 """
 import logging
 import random
+import pymysql
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
